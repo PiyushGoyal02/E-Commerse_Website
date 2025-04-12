2395497fc87b5781b47860c6069ae4d34512e7a2
PiyushGoyal02 <goyalp3542@gmail.com>
2025-04-12 21:06:55 +0530

    I have update code AdminHomePage.js

--- src/AdminSection/AdminHomePage.js
+++ src/AdminSection/AdminHomePage.js
@@ -1,28 +1,65 @@
-import "../AdminSectionCSS/AdminHomePageCSS.css"
+import "../AdminSectionCSS/AdminHomePageCSS.css";
 import AdminNavbar from "../Navbar-Sections/AdminNavbar";
+import AdminSideAllUsers from "./AdminSideAllUsers";
+import "../AdminSectionCSS/AdminSidebarCSS.css";
+import { MdOutlineLibraryAddCheck } from "react-icons/md";
+import { PiListHeartBold } from "react-icons/pi";
+import { TbCheckbox, TbUsersGroup } from "react-icons/tb";
+import { useState } from "react";
 import AdminOrderDetails from "./AdminOrderDetails";
 import AdminProductsList from "./AdminProductsList";
-import AdminSideAllUsers from "./AdminSideAllUsers";
-import AdminSidebar from "./AdminSidebar";
 
-function AdminHomePage(){
+function AdminHomePage() {
+    const [active, setActive] = useState("add");
+
     return (
         <div>
-            <AdminNavbar/>
+            <AdminNavbar />
             <div className="Sidebar-DifferentSections">
                 <div>
-                    <AdminSidebar/>
+                    <div className="AdminSidebardiv">
+                        <div
+                            className={`DivSection ${active === "add" ? "active" : ""}`}
+                            onClick={() => setActive("add")}
+                        >
+                            <MdOutlineLibraryAddCheck className="Icons" />
+                            <p className="Ptag-text">Add Products</p>
+                        </div>
+
+                        <div
+                            className={`DivSection ${active === "list" ? "active" : ""}`}
+                            onClick={() => setActive("list")}
+                        >
+                            <PiListHeartBold className="Icons" />
+                            <p className="Ptag-text">Product List</p>
+                        </div>
+
+                        <div
+                            className={`DivSection ${active === "order" ? "active" : ""}`}
+                            onClick={() => setActive("order")}
+                        >
+                            <TbCheckbox className="Icons" />
+                            <p className="Ptag-text">Orders</p>
+                        </div>
+
+                        <div
+                            className={`DivSection ${active === "users" ? "active" : ""}`}
+                            onClick={() => setActive("users")}
+                        >
+                            <TbUsersGroup className="Icons" />
+                            <p className="Ptag-text">All Users</p>
+                        </div>
+                    </div>
                 </div>
 
-                <div>
-                    <AdminSideAllUsers/>
-                    {/* <AdminOrderDetails/> */}
-                    {/* <AdminProductsList/> */}
+                <div className="AdminMainContent">
+                    {active === "users" && <AdminSideAllUsers />}
+                    {active === "order" && <AdminOrderDetails />}
+                    {active === "list" && <AdminProductsList />}
                 </div>
-
             </div>
         </div>
-    )
+    );
 }
 
 export default AdminHomePage;
