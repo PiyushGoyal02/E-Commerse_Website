0f083c726bfa655b3ae8a37cadf2014767362512
PiyushGoyal02 <goyalp3542@gmail.com>
2025-04-24 17:20:22 +0530

    Upadted node mailer code After User Signup

--- Backend/Mail/SendMailer.js
+++ Backend/Mail/SendMailer.js
@@ -18,9 +18,21 @@ const MailSender = async (email) => {
         let info = await transporter.sendMail({
             from: `"Green Cart" <${process.env.EMAIL_USER}>`,
             to: email,
-            subject: "Welcome to Green Cart!",
-            text: "Thank you for signing up. We're excited to have you on board!",
+            subject: "Welcome to Green Cart! 🌿",
+            text: `Hi there,
+
+            Welcome to the Green Cart family! 🛒🌱
+
+            We're truly excited to have you on board. At Green Cart, we believe in bringing you the freshest products, handpicked with care, and delivered with love. Your journey towards a healthier and eco-friendly lifestyle begins right here.
+
+            Feel free to explore our wide range of items made just for you. And don’t worry — we’re always here if you need help, support, or just a little shopping inspiration. 😉
+
+            Thank you for choosing us. Let’s grow together.
+
+            Warm regards,  
+            Team Green Cart`
         });
+
 
 
         console.log("Email sent successfully: ", info);
